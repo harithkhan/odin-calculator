768c483c390e97021dd8b59d0846e5142e09d81b
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2024-12-14 21:06:00 +0800

    feat: ensure number clicks show on display Create function to handle clicks of digit buttons. Add event listeners to buttons that call above function.

--- javascript.js
+++ javascript.js
@@ -20,7 +20,7 @@ let displayFirstNumber;
 let displayOperator;
 let displaySecondNumber;
 
-// Function that calls operator functions on 2 numbers
+// Function that calls calculator operator functions on 2 numbers
 function operator(mathOperator, firstNum, secondNum) {
     if (mathOperator === "+") {
         return add(firstNum, secondNum);
@@ -32,3 +32,57 @@ function operator(mathOperator, firstNum, secondNum) {
         return divide(firstNum, secondNum);
     };
 };
+
+// Functions that populate display when digit buttons are clicked
+
+const display = document.querySelector(".display");
+let displayValue = 0;
+function handleDigitClick(digit) {
+    if (displayValue === 0) {
+        displayValue = digit;
+    } else {
+        displayValue = displayValue * 10 + digit;
+    }
+    display.textContent = displayValue;
+    console.log(displayValue);
+}
+
+// Handle when "0" is clicked
+const zero = document.querySelector(".zero");
+zero.addEventListener("click", () => handleDigitClick(0));
+
+// Handle when "1" is clicked
+const one = document.querySelector(".one");
+one.addEventListener("click", () => handleDigitClick(1));
+
+// Handle when "2" is clicked
+const two = document.querySelector(".two");
+two.addEventListener("click", () => handleDigitClick(2));
+
+// Handle when "3" is clicked
+const three = document.querySelector(".three");
+three.addEventListener("click", () => handleDigitClick(3));
+
+// Handle when "4" is clicked
+const four = document.querySelector(".four");
+four.addEventListener("click", () => handleDigitClick(4));
+
+// Handle when "5" is clicked
+const five = document.querySelector(".five");
+five.addEventListener("click", () => handleDigitClick(5));
+
+// Handle when "6" is clicked
+const six = document.querySelector(".six");
+six.addEventListener("click", () => handleDigitClick(6));
+
+// Handle when "7" is clicked
+const seven = document.querySelector(".seven");
+seven.addEventListener("click", () => handleDigitClick(7));
+
+// Handle when "8" is clicked
+const eight = document.querySelector(".eight");
+eight.addEventListener("click", () => handleDigitClick(8));
+
+// Handle when "9" is clicked
+const nine = document.querySelector(".nine");
+nine.addEventListener("click", () => handleDigitClick(9));
