def87d780ebee11cf01ea6cc5e7cd1e160a8832a
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2024-12-14 15:57:31 +0800

    feat: add basic functions of a calculator Create add, subtract, multiply and divide functions that take 2 arguments.

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add (a, b) {
+    return a + b;
+};
+console.log(add(1, 1));
+
+function subtract(a, b) {
+    return a - b;
+};
+console.log(subtract(1, 1));
+
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(1, 1));
+
+function divide(a, b) {
+    return a / b;
+}
+console.log(divide(10, 5));
